44da66acc0a96883c95fd175e592628353f25907
Tony Ngninlabhe <tony.ngninlabhe@oit.edu>
2020-12-11 20:10:26 -0800

    fixed build

--- ProductService/Services/BusinessRules.cs
+++ ProductService/Services/BusinessRules.cs
@@ -6,7 +6,7 @@ public static class BusinessRules
     public static bool isOnSale(Product product) =>
         product.Name.ToLower().StartsWith(onSaleKey);
 
-    public static int lowInventoryCount = 2;
+    public static int lowInventoryCount = 3;
 
     public static string onSaleKey = "b";
 }
